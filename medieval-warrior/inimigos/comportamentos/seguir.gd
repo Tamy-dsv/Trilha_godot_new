fe502368a82e23572cb80327828dbe06ec395ca8
Tamara Venturas Fernandes <tamaraventuras25@gmail.com>
2025-05-15 17:07:03 -0400

    aula 15 de maio
    atualizando codigo

--- medieval-warrior/inimigos/comportamentos/seguir.gd
+++ medieval-warrior/inimigos/comportamentos/seguir.gd
@@ -1,21 +1,30 @@
-extends CharacterBody2D
+extends Node
 
 @export var speed = 1
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var inimigos: Inimigos
+var sprite: AnimatedSprite2D 
+
+
+func _ready():
+	inimigos = get_parent()
+	sprite = inimigos.get_node("AnimatedSprite2D") 
+	inimigos.health
+	pass
+	
 func _physics_process(delta: float) -> void:
 	var player_position = GameManeger.player_position
-	var diference = player_position - position
+	var diference = player_position - inimigos.position 
 	var input_vector = diference.normalized()
 	
-	
+
+	#input vector
+	inimigos.velocity = input_vector * speed * 100.0
+	inimigos.move_and_slide()
+  		
+		
 		# Girar o sprite 	
 	if input_vector.x > 0:
 		sprite.flip_h = false
 	elif input_vector.x < 0:		
 		sprite.flip_h = true	
-	
-	
-	#input vector
-	velocity = input_vector * speed * 100.0
-	
-	move_and_slide()
---
+++ medieval-warrior/inimigos/inimigos.gd
@@ -0,0 +1,4 @@
+class_name Inimigos
+extends Node2D
+
+@export var health: int = 10
